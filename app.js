58da23e2c8fc656bda0e647ff1c77d078beb9ab5
suryansh <csuryansh23@gmail.com>
2018-02-15 21:55:27 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('It works');
+    res.send('It works!');
 });
 
-const port = process.env.PORT || 5000;
+app.use('/auth',auth);
+
+const port = process.env.PORT || 5000 ;
 
 app.listen(port,()=>{
     console.log('Server is up on port ' + port);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI : 'mongodb://suryansh:hello@ds237848.mlab.com:37848/storybook-dev',
+    googleClientID:'957682392946-0r1o62lqd5g126j795u5pqrnqfds2nhc.apps.googleusercontent.com',
+    googleClientSecret: 'mirYPSOEh4XY662rZkqfaQIF'
+}
