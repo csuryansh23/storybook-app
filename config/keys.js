71a6a847a61e24ad4741117d20ad3affa2647a00
suryansh <csuryansh23@gmail.com>
2018-02-16 13:05:33 +0530

    one last time

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI : 'mongodb://suryansh:hello@ds237848.mlab.com:37848/storybook-dev',
-    googleClientID:'957682392946-0r1o62lqd5g126j795u5pqrnqfds2nhc.apps.googleusercontent.com',
-    googleClientSecret: 'K3ELiePVrVpZmvquFVU5A5oH'
+    googleClientID:'957682392946-dbhp0ts9jliaaplqnpkm68lae5l2s2ta.apps.googleusercontent.com',
+    googleClientSecret: 'dGLeJ-Y-yiN9a2P00UJ5IsRi'
 }
