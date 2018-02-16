6385a5aad905d40c468d117381e26acc955c9bd3
suryansh <csuryansh23@gmail.com>
2018-02-16 11:38:29 +0530

    port changed to see what happens

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/',(req,res)=>{
  
 app.use('/auth',auth);
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 4000 ;
 
 app.listen(port,()=>{
     console.log('Server is up on port ' + port);
